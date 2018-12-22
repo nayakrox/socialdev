264071de5feec308f878a9b710b638242a831fca
Kunal Nayak <nayakrox@gmail.com>
2018-12-22 20:48:26 +0530

    added validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,25 +6,37 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 // Load User model
 const User = require("../../models/User");
 
-// @route GET api/users/test
-// @desc Tests users route
-// @access Public
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
 router.get("/test", (req, res) => {
   res.json({
     msg: "Users works!"
   });
 });
 
-// @route GET api/users/register
-// @desc Register a user
-// @access Public
+// @route   POST api/users/register
+// @desc    Register a user
+// @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists!" });
+      errors.email = "Email already exists!";
+      return res.status(400).json({ errors });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200",
@@ -52,10 +64,17 @@ router.post("/register", (req, res) => {
   });
 });
 
-// @route GET api/users/login
-// @desc Login a user /Returning JWT Token
-// @access Public
+// @route   POST api/users/login
+// @desc    Login a user /Returning JWT Token
+// @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -63,7 +82,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for User
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -85,17 +105,16 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({
-          password: "Password incorrect"
-        });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
 });
 
-// @route GET api/users/current
-// @desc Return current user
-// @access Private
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
 router.get(
   "/current",
   passport.authenticate("jwt", { session: false }),
